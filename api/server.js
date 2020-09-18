92a833f56bf97c4bef202dfdaa26015498e31345
aligolji <aligolji@users.noreply.github.com>
2020-09-18 15:44:57 -0700

    auth model, auth middleware, and registration operation complete. needs testing

--- api/server.js
+++ api/server.js
@@ -2,17 +2,40 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const db = require('../database/dbConfig');
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'when there is a doubt, there is no doubt',
+    store: new KnexSessionStore({
+        knex: db,
+        createtable: true,
+    }),
+}));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
+    //post to /api/auth/register --> register new user
+    //post to /api/auth/login --> login user
+    //get to /api/jokes --> runs authenticate middleware --  once authenticated, retrieves jokes
+server.use((err, req, res, next) => {
+    console.log(err);
+
+    res.status(500).json({
+        message: 'Something went wrong.'
+    });
+});
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const authMiddleware = require('./authenticate-middleware');
+const Users = require('./auth-model');
 
-router.post('/register', (req, res) => {
+
+router.post('/register', async (req, res, next) => {
   // implement registration
+  try {
+    const { username, password } = req.body;
+    const user = await URLSearchParams.findBy({ username }).first()
+
+    if (user) {
+      return res.status(409).json({
+        message: 'Username is already taken.',
+      })
+    }
+    const newUser = await Users.add({
+      username,
+      password: await bcrypt.hash(password, 14),
+    })
+    res.status(201).json(newUser)
+  }
+  
+  catch (err) {
+    next(err)
+  }
+
 });
 
 router.post('/login', (req, res) => {
